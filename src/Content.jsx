8c3cc7010c79aa3782a6afddc2ec749cc5d6d0ea
Manish Bag <manishbag007@gmail.com>
2024-01-23 11:48:42 +0530

    useState hook - chapter 6

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 const Content = () => {
+  const [name, setName] = useState("Manish");
+  const [count, setCount] = useState(0);
+  const handleNameChange = () => {
+    const names = ["Manish", "Arpit", "Sid"];
+    const randomIdx = Math.floor(Math.random() * 3);
+    setName(names[randomIdx]);
+  };
+  const increaseCount = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
   return (
     <main>
-      <p>HELLO</p>
+      <p>HELLO {name}!</p>
+      <button onClick={handleNameChange}>CHANGE NAME</button>
+      <br></br>
+      <br></br>
+      <button onClick={increaseCount}>{count}</button>
     </main>
   );
 };
