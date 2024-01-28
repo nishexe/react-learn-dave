71b259081b2d7a11cc1c43ffbc41bd513e31e208
Manish Bag <manishbag007@gmail.com>
2024-01-28 11:27:06 +0530

    fixed showing list items when unable to fetch the data

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,19 @@
-const Footer = ({ length, isLoading }) => {
-  return (
-    <footer>
-      {isLoading ? (
-        <p>Loading items!</p>
-      ) : (
-        <p>
-          You have {length} {length > 1 ? "items" : "item"} in the list
-        </p>
-      )}
-    </footer>
-  );
+const Footer = ({ length, isLoading, fetchError }) => {
+  if (fetchError) {
+    return <footer></footer>;
+  } else {
+    return (
+      <footer>
+        {isLoading ? (
+          <p>Loading items!</p>
+        ) : (
+          <p>
+            You have {length} {length > 1 ? "items" : "item"} in the list
+          </p>
+        )}
+      </footer>
+    );
+  }
 };
 
 export default Footer;
